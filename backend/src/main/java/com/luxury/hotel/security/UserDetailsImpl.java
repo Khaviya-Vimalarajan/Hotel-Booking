44ad58af3576e1b38058fdfe0c215ba18a017778
viya-vimal <khaviyavimal6@gmail.com>
2026-06-03 11:45:51 +0530

    feat: Setup JWT authentication and Spring Security configurations

---
+++ backend/src/main/java/com/luxury/hotel/security/UserDetailsImpl.java
@@ -0,0 +1,90 @@
+package com.luxury.hotel.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.luxury.hotel.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+@AllArgsConstructor
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private Long id;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String profileImage;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsImpl build(User user) {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().name());
+        
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPhone(),
+                user.getProfileImage(),
+                Collections.singletonList(authority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
