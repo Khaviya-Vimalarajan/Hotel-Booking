738be45383a95b71299934cb0ed091652696a15d
viya-vimal <khaviyavimal6@gmail.com>
2026-06-17 11:45:51 +0530

    feat: Setup backend main application runner and config properties

---
+++ backend/src/main/java/com/luxury/hotel/ResortManagementApplication.java
@@ -0,0 +1,11 @@
+package com.luxury.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResortManagementApplication.class, args);
+    }
+}
